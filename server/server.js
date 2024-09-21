04e370fad9eca02f04d3b66b8c2e5c44724edf30
KAVYA-KAMALE <133418422+KAVYA-KAMALE@users.noreply.github.com>
2024-09-21 09:12:27 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,27 +3,20 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const studentRoutes = require('./routes/studentRoutes');
-const attendanceRoutes = require('./routes/attendanceRoutes');  // Correct route
+const attendanceRoutes = require('./routes/attendanceRoutes');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
 
-// Connect to MongoDB
 connectDB();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/students', studentRoutes);
 app.use('/api/attendance', attendanceRoutes);
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-

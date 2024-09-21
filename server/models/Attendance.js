730a651c5e9a2b05aad88708a728769daa922cf7
KAVYA-KAMALE <133418422+KAVYA-KAMALE@users.noreply.github.com>
2024-09-21 09:13:16 +0530

    Update Attendance.js

--- server/models/Attendance.js
+++ server/models/Attendance.js
@@ -1,23 +1,23 @@
-// Attendance.js
 const mongoose = require('mongoose');
 
 const attendanceSchema = new mongoose.Schema({
-    uniqueId: {
+    rollNo: {
         type: String,
-        required: true,
-        unique: true // Ensuring each uniqueId can only have one attendance record
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Present', 'Absent'],  // Default enum values for attendance
+        required: true
     },
     timestamp: {
         type: Date,
         default: Date.now
-    },
-    status: {
-        type: String,
-        enum: ['Present', 'Absent'],
-        default: 'Present'
     }
-}, {
-    collection: 'attendances'
 });
 
 module.exports = mongoose.model('Attendance', attendanceSchema);
